9d433b145f9a56da0f252cb559e375a8f04849cf
Louise <louise.beh@gmail.com>
2015-01-28 17:11:55 +0000

    added method for user input of student name

--- directory.rb
+++ directory.rb
@@ -1,35 +1,3 @@
-# let's put all students into an array
-students = [
-  {:name => "Alex Parkinson", :cohort => :February},
-  {:name => "Carrie Christenson", :cohort => :February},
-  {:name => "Costas Sarris", :cohort => :February},
-  {:name => "Diego Romero", :cohort => :February},
-  {:name => "Ed Brown", :cohort => :February},
-  {:name => "Erik Brown", :cohort => :February},
-  {:name => "Gabe Arafa", :cohort => :February},
-  {:name => "Guido Fawkes", :cohort => :February},
-  {:name => "Jow Newman", :cohort => :February},
-  {:name => "Julian Veling", :cohort => :February},
-  {:name => "Kate Beavis", :cohort => :February},
-  {:name => "Kevin Lanzon", :cohort => :February},
-  {:name => "Louise Beh", :cohort => :February},
-  {:name => "Loris Fochesato", :cohort => :February},
-  {:name => "Margherita Serino", :cohort => :February},
-  {:name => "Meads Chalcroft", :cohort => :February},
-  {:name => "Paul Fitz", :cohort => :February},
-  {:name => "Phil Brockwell", :cohort => :February},
-  {:name => "Ptolemy Barnes", :cohort => :February},
-  {:name => "Riz Ali", :cohort => :February},
-  {:name => "Sanda Golcea", :cohort => :February},
-  {:name => "Sean Haughton", :cohort => :February},
-  {:name => "Sebastien Pires", :cohort => :February},
-  {:name => "Thomas Strothjohann", :cohort => :February},
-  {:name => "Tom Coakes", :cohort => :February},
-  {:name => "Tom Bratto", :cohort => :February},
-  {:name => "Vanessa Virgitti", :cohort => :February},
-  {:name => "Yannick Brunner", :cohort => :February}
-]
-
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
@@ -45,7 +13,26 @@ def print_footer(students)
   puts "Overall, we have #{students.length} great students"
 end
 
-#nothing happens until we call the methods
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {:name => name, :cohort => :February}
+    puts "Now we have #{students.length} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  #return the arrange of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
