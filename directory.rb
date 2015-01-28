8558c49d719b7ca21edec358453a7bf65e132abe
Louise <louise.beh@gmail.com>
2015-01-28 15:19:08 +0000

    code refactored

--- directory.rb
+++ directory.rb
@@ -1,36 +1,41 @@
-#First, we print the list of students
+# let's put all students into an array
+students = [
+  "Alex Parkinson",
+  "Carrie Christenson",
+  "Costas Sarris",
+  "Diego Romero",
+  "Ed Brown",
+  "Erik Brown",
+  "Gabe Arafa",
+  "Guido Fawkes",
+  "Jow Newman",
+  "Julian Veling",
+  "Kate Beavis",
+  "Kevin Lanzon",
+  "Louise Beh",
+  "Loris Fochesato",
+  "Margherita Serino",
+  "Meads Chalcroft",
+  "Paul Fitz",
+  "Phil Brockwell",
+  "Ptolemy Barnes",
+  "Riz Ali",
+  "Sanda Golcea",
+  "Sean Haughton",
+  "Sebastien Pires",
+  "Thomas Strothjohann",
+  "Tom Coakes",
+  "Tom Bratto",
+  "Vanessa Virgitti",
+  "Yannick Brunner"
+]
+# and then print them
 puts "The students of my cohort at Makers Academy"
 puts "-------------"
-puts "Alex Parkinson"
-puts "Carrie Christenson"
-puts "Costas Sarris"
-puts "Diego Romero"
-puts "Ed Brown"
-puts "Erik Brown"
-puts "Gabe Arafa"
-puts "Guido Fawkes"
-puts "Jow Newman"
-puts "Julian Veling"
-puts "Kate Beavis"
-puts "Kevin Lanzon"
-puts "Louise Beh"
-puts "Loris Fochesato"
-puts "Margherita Serino"
-puts "Meads Chalcroft"
-puts "Paul Fitz"
-puts "Phil Brockwell"
-puts "Ptolemy Barnes"
-puts "Riz Ali"
-puts "Sanda Golcea"
-puts "Sean Haughton"
-puts "Sebastien Pires"
-puts "Thomas Strothjohann"
-puts "Tom Coakes"
-puts "Tom Bratto"
-puts "Vanessa Virgitti"
-puts "Yannick Brunner"
-#Finally, we print the total
-print "Overall, we have "
-#It's important that print () doesn't add new line characters
-print 28
-puts " great students"
+
+students.each do |student|
+  puts student
+end
+
+# finally, we print the total
+puts "Overall, we have #{students.length} great students"
