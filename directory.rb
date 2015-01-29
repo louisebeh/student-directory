0f4165547c307605d03bfdc16c0b574b7f22b9fb
Louise <louise.beh@gmail.com>
2015-01-29 13:59:00 +0000

    added method to load data from students.csv

--- directory.rb
+++ directory.rb
@@ -15,12 +15,6 @@ def print_footer
   puts "Overall, we have #{@students.length} great students"
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "9. Exit"
-end
 
 def input_students
   puts "Please enter the names of the students"
@@ -35,6 +29,14 @@ def input_students
     # get another name from the user
     name = gets.chomp
   end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
 end
 
 def show_students
@@ -55,6 +57,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {:name => name, :cohort => cohort.to_sym}
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
@@ -63,6 +74,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
